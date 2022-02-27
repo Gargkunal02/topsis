3c138adce87a8b35d6edb2f814d726512311d8d6
Kunal <gargkunal02@gmail.com>
2022-02-27 23:59:02 +0530

    changes

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
-from setuptools import setup, find_packages
-import codecs
-import os
-
-VERSION = '0.0.13'
-DESCRIPTION = 'Streaming video data via networks'
-
-# Setting up
+from distutils.core import setup
 setup(
-    name="KunalGarg_101903683",
-    version=VERSION,
-    author="NeuralNine (Florian Dedov)",
-    author_email="<mail@neuralnine.com>",
-    description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    packages=find_packages(),
-    install_requires=['pandas','numpy','math','copy','sys','os'],
-    classifiers=["Programming Language :: Python :: 3"]
+  name = 'KunalGarg_101903683',         # How you named your package folder (MyLib)
+  packages = ['KunalGarg_101903683'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  author = 'Kunal garg',                   # Type in your name
+  author_email = 'kgarg2_be19@thapar.edu',      # Type in your E-Mail
+  url = 'https://github.com/Gargkunal02/topsis.git',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Gargkunal02/topsis/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  install_requires=['pandas','numpy','math','copy','sys','os']
 )
